8476a0244b3df3c60d49273d32211db6359a6f1b
juliocornejo <elimino.gente.por.dinero@gmail.com>
2019-12-15 18:46:05 -0300

    first commit

---
+++ src/main/java/com/ragnax/arrastrameorquestador/ArrastrameOrquestadorApplication.java
@@ -0,0 +1,13 @@
+package com.ragnax.arrastrameorquestador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrastrameOrquestadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrastrameOrquestadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ragnax/arrastrameorquestador/ArrastrameOrquestadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragnax.arrastrameorquestador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrastrameOrquestadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
